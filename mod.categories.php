562af2cf3651ab54296513a9b9727794e2408b4f
Bjorn Borresen <mail@bybjorn.com>
2010-07-01 10:58:16 +0200

    added support for {category_description} + parameter for only getting root categoires (that is no children)

--- mod.categories.php
+++ mod.categories.php
@@ -18,17 +18,23 @@ class Categories {
 		$this->EE =& get_instance(); // Make a local reference to the ExpressionEngine super object
 		
 		$category_group_id = intval($this->_get_param('category_group_id'));
+
 		if($category_group_id == 0)
 		{
 			return $this->EE->output->show_user_error('general', "{exp:categories} required parameter missing: category_group_id (can only be a specific id)");	
-		}		
-		
+		}
+		$children = ($this->_get_param('children', 'y') == 'y');
+				
 		$url_title = $this->_get_param('url_title');
 				
 		$where_params = array('group_id' => $category_group_id);
 		if($url_title != "")
 		{
 			$where_params['cat_url_title'] = $url_title;
+		}
+		if(!$children)
+		{
+			$where_params['parent_id'] = 0;
 		}
 		
 		$query = $this->EE->db->get_where('categories', $where_params);
@@ -39,7 +45,8 @@ class Categories {
 			$vars[] = array(
 				'category_name' => $row->cat_name,
 				'category_url_title' => $row->cat_url_title,
-				'category_image' => $row->cat_image			
+				'category_image' => $row->cat_image,
+				'category_description' => $row->cat_description,			
 			);						
 		}
 		$this->return_data = $this->EE->TMPL->parse_variables($this->EE->TMPL->tagdata, $vars);
