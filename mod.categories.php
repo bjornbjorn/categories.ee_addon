102bbe9dae37bb4be6efbb9c31a69d6c73263611
Bjorn Borresen <mail@bybjorn.com>
2010-07-02 15:24:47 +0200

    added support for nested/linear styles + {children} tag (only one level deep for now)

--- mod.categories.php
+++ mod.categories.php
@@ -39,24 +39,64 @@ class Categories {
 		
 		$query = $this->EE->db->get_where('categories', $where_params);
 		
-		$vars = array(); 
+		$vars = array();
+		$root_categories = array();
+		$children_categories = array();
+		 
 		foreach($query->result() as $row)
 		{
-			$vars[] = array(
-				'category_name' => $row->cat_name,
-				'category_url_title' => $row->cat_url_title,
-				'category_image' => $row->cat_image,
-				'category_description' => $row->cat_description,			
-			);						
+			if($row->parent_id==0)
+			{
+				$root_categories[] = $this->_get_category_arr($row);
+			}
+			else
+			{
+				if(!isset($children_categories[$row->parent_id]))
+				{
+					$children_categories[$row->parent_id] = array();
+				}
+				$children_categories[$row->parent_id][] = $this->_get_category_arr($row, TRUE);
+			}
 		}
+		
+		foreach($root_categories as $cat)
+		{
+			if(isset($children_categories[$cat['category_id']]))	// if has children
+			{
+				$cat['children'] = $children_categories[$cat['category_id']];
+				$cat['has_children'] = TRUE;
+			}
+			else
+			{
+				$cat['children'] = array();
+				$cat['has_children'] = FALSE;
+			}
+			
+			$vars[] = $cat;
+		}
+		 							
 		$this->return_data = $this->EE->TMPL->parse_variables($this->EE->TMPL->tagdata, $vars);
 		return $this->return_data;		
 	}
-		
-	function _get_category_group_id()
+	
+	/**
+	 * Get a template-ready array for categories
+	 * 
+	 * @param unknown_type $row
+	 */
+	function _get_category_arr($row, $is_child=FALSE)
 	{
-		
+		$prefix = ($is_child?'child_':'');
+		return array(
+				$prefix.'category_id' => $row->cat_id,
+				$prefix.'category_name' => $row->cat_name,
+				$prefix.'category_url_title' => $row->cat_url_title,
+				$prefix.'category_image' => $row->cat_image,
+				$prefix.'category_description' => $row->cat_description,			
+			);	
 	}
+		
+
 	
 	/**
      * Helper function for getting a parameter
